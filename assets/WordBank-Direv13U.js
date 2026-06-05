0ca6f0f9e73dbaf1229204d779a510e11179bb38
ccherosc <ccherosc@users.noreply.github.com>
2026-06-05 22:47:37 +0000

    deploy: publish PalabraCade GitHub Pages build

---
+++ assets/WordBank-Direv13U.js
@@ -0,0 +1,1 @@
+function a(r){const t=[...r];for(let o=t.length-1;o>0;o--){const n=Math.floor(Math.random()*(o+1));[t[o],t[n]]=[t[n],t[o]]}return t}export{a as s};
--- src/games/conjugation-castle/ConjugationCastle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ComingSoon from '../../components/ComingSoon.jsx';
-import meta from './metadata.js';
-
-export default function ConjugationCastle() {
-  return <ComingSoon game={meta} />;
-}
